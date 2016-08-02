49fed1293523aa427e35a1228c52e97587131828
Rafał Starosta <rafal.starosta@poczta.pl>
2016-08-02 21:37:03 +0200

    Add files

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+
+    <!-- meta -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <!-- javascript -->
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="form-group" id="box">
+
+                <!-- Left side -->
+
+                <div class="col-md-3 col-md-offset-4">
+                    <div class="checking checking-red">
+                        <input type="checkbox" id="check-blue">
+                        <label for="check-blue">&nbsp;&nbsp;Red</label>
+                    </div>
+                    <div class="checking checking-blue">
+                        <input type="checkbox" id="check-red">
+                        <label for="check-red">&nbsp;&nbsp;Blue</label>
+                    </div>
+                    <div class="checking checking-black">
+                        <input type="checkbox" id="check-black">
+                        <label for="check-black">&nbsp;&nbsp;Black</label>
+                    </div>
+                    <div class="checking checking-green">
+                        <input type="checkbox" id="check-green">
+                        <label for="check-green">&nbsp;&nbsp;Green</label>
+                    </div>
+                </div>
+            </div>
+
+                <!-- Right side -->
+
+            <div class="form-group" id="box-right"
+                <div class="col-md-3 col-md-offset-5">
+                    <div class="checking-two checking-red">
+                        <input type="checkbox" id="check-blue-two">
+                        <label for="check-blue-two">&nbsp;&nbsp;Red</label>
+                    </div>
+                    <div class="checking-two checking-blue">
+                        <input type="checkbox" id="check-red-two">
+                        <label for="check-red-two">&nbsp;&nbsp;Blue</label>
+                    </div>
+                    <div class="checking-two checking-black">
+                        <input type="checkbox" id="check-black-two">
+                        <label for="check-black-two">&nbsp;&nbsp;Black</label>
+                    </div>
+                    <div class="checking-two checking-green">
+                        <input type="checkbox" id="check-green-two">
+                        <label for="check-green-two">&nbsp;&nbsp;Green</label>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
